c8de981f3eace6b96ad7182f90a43d700f883f64
fengjie <32606734+ChNone@users.noreply.github.com>
2018-03-23 18:51:08 +0800

    1
    add a new function

---
+++ src/java/learGitHub/FindBig.java
@@ -0,0 +1,15 @@
+package learGitHub;
+
+public class FindBig {
+
+    public void findBigest(int[] list){
+        int temp = 0;
+        for (int i = 0;i<list.length;i++){
+            if(list[i] > temp) {
+                temp = list[i];
+            }
+        }
+        System.out.println(temp);
+    }
+
+}
